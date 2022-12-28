2ebef088fc18b92db369e717973786952616b44b
fankx <1225452950@qq.com>
2022-12-28 14:34:29 +0800

    feat:导出功能

---
+++ src/main/java/com/ns/entity/dto/SysUserDto.java
@@ -0,0 +1,29 @@
+package com.ns.entity.dto;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@ColumnWidth(25)
+public class SysUserDto {
+    @ExcelProperty("用户名")
+    private String username;
+    @ExcelProperty("昵称")
+    private String nickname;
+    @ExcelProperty("邮箱")
+    private String email;
+    @ExcelProperty("电话")
+    private String phone;
+    @ExcelProperty("地址")
+    private String address;
+    @ExcelProperty("创建时间")
+    private Date createTime;
+    @ExcelProperty("头像")
+    private String avatarUrl;
+    @ExcelProperty("角色")
+    private String role;
+}
